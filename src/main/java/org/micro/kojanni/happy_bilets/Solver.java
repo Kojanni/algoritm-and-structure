ff620bf66db094729b99940e857d3e33ad39935a
ekochetkova <ekochetkova@diasoft.ru>
2025-07-28 20:56:41 +0300

    20250726 подсчет счастливых билетов

--- src/main/java/org/micro/kojanni/happy_bilets/Solver.java
+++ src/main/java/org/micro/kojanni/happy_bilets/Solver.java
@@ -1,27 +1,58 @@
 package org.micro.kojanni.happy_bilets;
 
 public class Solver {
+    static final int MAX_N = 100;
+    static final int MAX_S = 100;
+    static long[][] memory = new long[MAX_N + 1][MAX_S + 1];
 
-    public String run(String[] args)
-    {
-        int n =  Integer.parseInt(args[0]);
-        return String.valueOf(Tickets(n));
-    }
-
-    private Long Tickets(int n) {
-        switch(n)
-        {
-            case 1: return 10L;
-            case 2: return 670L;
-            case 3: return 55252L;
-            case 4: return 4816030L;
-            case 5: return 432457640L;
-            case 6: return 39581170420L;
-            case 7: return 3671331273480L;
-            case 8: return 343900019857310L;
-            case 9: return 32458256583753952L;
-            case 10:return 3081918923741896840L;
+    public Solver() {
+        super();
+        //инициализация: -1 - не вычисленно
+        for (int i = 0; i <= MAX_N; i++) {
+            for (int j = 0; j <= MAX_S; j++) {
+                memory[i][j] = -1L;
+            }
         }
-        return null;
+
     }
+
+    public String run(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        return String.valueOf(tickets(n));
+    }
+
+    private Long tickets(int n) {
+        long totalTickets = 0;
+
+        int maxPossibleSum = 9 * n;
+        for (int s = 0; s <= maxPossibleSum; s++) {
+            long count = calc(n, s);
+            totalTickets += count * count;  // Квадрат количества комбинаций
+        }
+
+        return totalTickets;
+    }
+
+    private long calc(int n, int s) {
+        if (n < 0 || s < 0) {
+            return 0;
+        }
+        if (memory[n][s] != -1) {
+            return memory[n][s];
+        }
+        if (n == 0) {
+            return (s == 0) ? 1 : 0;
+        }
+
+        long count = 0;
+        for (int i = 0; i < 10; i++) {
+            if (s - i >= 0) {
+                count += calc(n - 1, s - i);
+            }
+        }
+        memory[n][s] = count;
+
+        return count;
+    }
+
 }
